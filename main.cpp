3d72ba413f97bc754e64e4c38faa079733df8477
Nick Goodson <jng2ng@mst.edu>
2020-09-02 23:24:42 -0500

    Remove commented winmain

--- main.cpp
+++ main.cpp
@@ -1,13 +1,5 @@
 #define SDL_MAIN_HANDLED
 #include "GB.h"
-//#include <Windows.h>
-
-// int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR pCmdLine, int nCmdShow) {
-// 	GB gameboy(__argv[1]);
-// 	gameboy.execute();
-//     gameboy.mem->save();
-// 	return 0;
-// }
 
 int main(int argc, char* argv[])
 {
