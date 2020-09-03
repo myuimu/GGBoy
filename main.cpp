19e93463d8b2fe5be2aa00abcad56d4509f2711f
Nick Goodson <jng2ng@mst.edu>
2020-09-02 23:23:26 -0500

    Remove Windows specific files

--- main.cpp
+++ main.cpp
@@ -1,8 +1,17 @@
+#define SDL_MAIN_HANDLED
 #include "GB.h"
-#include <Windows.h>
+//#include <Windows.h>
 
-int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR pCmdLine, int nCmdShow) {
-	GB gameboy(__argv[1]);
+// int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE, LPSTR pCmdLine, int nCmdShow) {
+// 	GB gameboy(__argv[1]);
+// 	gameboy.execute();
+//     gameboy.mem->save();
+// 	return 0;
+// }
+
+int main(int argc, char* argv[])
+{
+  	GB gameboy(__argv[1]);
 	gameboy.execute();
     gameboy.mem->save();
 	return 0;
