96721eaaf83182e5e28d348e0f5de7209f9cde28
Nick Goodson <jng2ng@mst.edu>
2020-09-04 01:32:57 -0500

    Sort sprites before drawing

--- GB_GPU.h
+++ GB_GPU.h
@@ -3,18 +3,35 @@
 #include "GB_CONST.h"
 #include <stdio.h>
 #include <chrono>
+#include <vector>
+#include <algorithm>
+
+struct sprite
+{
+    uint8_t xPos;
+    uint8_t yPos;
+    uint8_t tileLocation;
+    uint8_t attributes;
+};
+
+bool spriteSort(const sprite& lhs, const sprite& rhs)
+{
+    if (lhs.xPos == rhs.xPos)
+        return lhs.tileLocation > rhs.tileLocation;
+    return lhs.xPos > rhs.xPos;
+}
 
 class GB_GPU {
     public:
-        //std::shared_ptr<std::array<unsigned char,0xFFFF>> memory;
+        //std::shared_ptr<std::array<uint8_t,0xFFFF>> memory;
         std::shared_ptr<GB_MEM> memory;
 
         unsigned int currentCycle = 0;
-        unsigned char scale = 3;
+        uint8_t scale = 3;
         SDL_Window* window = nullptr;
         SDL_Surface* screenSurface = nullptr;
         SDL_Surface* gameSurface = nullptr;
-        unsigned char* pixels = nullptr;
+        uint8_t* pixels = nullptr;
 
         GB_GPU()
         {
@@ -49,7 +66,7 @@ class GB_GPU {
                     SDL_Log("SDL_CreateRGBSurface() failed: %s", SDL_GetError());
                     exit(1);
                 }
-                pixels = (unsigned char*)gameSurface -> pixels;
+                pixels = (uint8_t*)gameSurface -> pixels;
             }
             
         }
@@ -96,7 +113,7 @@ class GB_GPU {
             else //Advance line
             {
                 memory->write(LY, memory->read(LY) + 1);
-                unsigned short line = memory->read(LY);
+                uint16_t line = memory->read(LY);
                 currentCycle -= CYCLES_PER_LINE;
                 if (line <= 144) //Draw line on screen
                 {
@@ -151,18 +168,18 @@ class GB_GPU {
             }
         }
 
-        void drawTiles(unsigned short line)
+        void drawTiles(uint16_t line)
         {
-            unsigned short displayAddress = 0;
-            unsigned short dataStart = 0;
+            uint16_t displayAddress = 0;
+            uint16_t dataStart = 0;
             bool usingWindow = false;
             bool unsign = true;
 
             //Background area and window location
-            unsigned char scrollY = memory->read(SCY);
-            unsigned char scrollX = memory->read(SCX);
-            unsigned char windowY = memory->read(WY);
-            unsigned char windowX = memory->read(WX) - 7;
+            uint8_t scrollY = memory->read(SCY);
+            uint8_t scrollX = memory->read(SCX);
+            uint8_t windowY = memory->read(WY);
+            uint8_t windowX = memory->read(WX) - 7;
 
             //Check if window is enabled
             if(memory->read(LCDC) & (1 << 5))
@@ -197,36 +214,36 @@ class GB_GPU {
 
             //Total tile area: 32x32 tiles (tile = 8x8 pixels)
             //Screen area: 20x18 tiles
-            unsigned char yPos = 0;
+            uint8_t yPos = 0;
             if(!usingWindow)
                 yPos = scrollY + line;
             else
                 yPos = line - windowY;
             
             //Which vertical tile is being used
-            unsigned short tileRow = (((unsigned char)(yPos/8))*32);
+            uint16_t tileRow = (((uint8_t)(yPos/8))*32);
 
             for(int i = 0; i < 160; i++)
             {
-                unsigned char xPos = i + scrollX;
+                uint8_t xPos = i + scrollX;
 
                 //Put x in window space if using window
                 if(usingWindow && (i >= windowX))
                     xPos = i - windowX;
 
                 //Which horizontal tile is being used
-                unsigned short tileCol = (xPos/8);
+                uint16_t tileCol = (xPos/8);
                 short tileNum;
 
                 //Get tile ID number
-                unsigned short tileAddress = displayAddress + tileRow + tileCol;
+                uint16_t tileAddress = displayAddress + tileRow + tileCol;
                 if(unsign)
-                    tileNum = (unsigned char)memory->read(tileAddress);
+                    tileNum = (uint8_t)memory->read(tileAddress);
                 else
                     tileNum = (char)memory->read(tileAddress);
 
                 //Get memory location of tile ID
-                unsigned short tileLocation = dataStart;
+                uint16_t tileLocation = dataStart;
 
                 if(unsign)
                     tileLocation += (tileNum * 16);
@@ -234,10 +251,10 @@ class GB_GPU {
                     tileLocation += ((tileNum + 128) * 16);
                 
                 //Get the correct line of the tile to draw
-                unsigned char column = yPos % 8;
+                uint8_t column = yPos % 8;
                 column *= 2; //Each line is two bytes
-                unsigned char data1 = memory->read(tileLocation + column);
-                unsigned char data2 = memory->read(tileLocation + column + 1);
+                uint8_t data1 = memory->read(tileLocation + column);
+                uint8_t data2 = memory->read(tileLocation + column + 1);
 
                 int colorBit = xPos % 8;
                 colorBit -= 7;
@@ -248,13 +265,13 @@ class GB_GPU {
                 colorNum <<= 1;
                 colorNum |= (data1 & (1 << colorBit)) >> colorBit;
 
-                unsigned char col = getColor(colorNum, 0xFF47);
+                uint8_t col = getColor(colorNum, 0xFF47);
                 if (xPos % 8 == 7 && (col == 0)) {}
                     //col += 2;
 
-                unsigned char red = 0;
-                unsigned char blue = 0;
-                unsigned char green = 0;
+                uint8_t red = 0;
+                uint8_t blue = 0;
+                uint8_t green = 0;
 
                 switch(col)
                 {
@@ -285,25 +302,41 @@ class GB_GPU {
             }
         }
 
-        void drawSprites(unsigned short line)
+        void drawSprites(uint16_t line)
         {
             bool tallSprites = false;
             if((memory->read(LCDC) & 0x4) == 0x4)
                 tallSprites = true;
 
-            for(int sprite = 0; sprite < 40; sprite++)
+            std::vector<sprite> sprites;
+
+            for(int i = 0; i < 40; i++)
             {
-                //Sprite takes 4 bytes in sprite attribute table
-                unsigned char index = sprite*4;
-                unsigned char yPos = memory->read(0xFE00+index) - 16;
-                unsigned char xPos = memory->read(0xFE00+index+1) - 8;
-                unsigned char tileLocation = memory->read(0xFE00+index+2);
-                unsigned char attributes = memory->read(0xFE00+index+3);
+                int index = i*4;
+
+                sprite s;
+
+                s.yPos = memory->read(0xFE00+index) - 16;
+                s.xPos = memory->read(0xFE00+index+1) - 8;
+                s.tileLocation = memory->read(0xFE00+index+2);
+                s.attributes = memory->read(0xFE00+index+3);
+
+                sprites.push_back(s);
+            }
+
+            std::sort(sprites.begin(), sprites.end(), spriteSort);
+
+            for(auto s : sprites)
+            {
+                uint8_t yPos = s.yPos;
+                uint8_t xPos = s.xPos;
+                uint8_t tileLocation = s.tileLocation;
+                uint8_t attributes = s.attributes;
 
                 bool yFlip = ((attributes & 0x40) == 0x40);
                 bool xFlip = ((attributes & 0x20) == 0x20);
             
-                unsigned char ySize = 8;
+                uint8_t ySize = 8;
                 if(tallSprites)
                 {
                     ySize = 16;
@@ -320,9 +353,9 @@ class GB_GPU {
                     }
 
                     spriteLine *= 2;
-                    unsigned short dataAddress = (0x8000 + (tileLocation * 16)) + spriteLine;
-                    unsigned char data1 = memory->read(dataAddress);
-                    unsigned char data2 = memory->read(dataAddress+1);
+                    uint16_t dataAddress = (0x8000 + (tileLocation * 16)) + spriteLine;
+                    uint8_t data1 = memory->read(dataAddress);
+                    uint8_t data2 = memory->read(dataAddress+1);
 
                     for(int tilePixel = 7; tilePixel >= 0; tilePixel--)
                     {
@@ -338,16 +371,16 @@ class GB_GPU {
                         colorNum <<= 1;
                         colorNum |= (data1 & (1 <<colorBit)) >> colorBit;
 
-                        unsigned short colorAddress = (attributes & (1 << 4)) >> 4 ? 0xFF49 : 0xFF48;
-                        unsigned char color = getColor(colorNum, colorAddress);
+                        uint16_t colorAddress = (attributes & (1 << 4)) >> 4 ? 0xFF49 : 0xFF48;
+                        uint8_t color = getColor(colorNum, colorAddress);
 
                         //White is transparent for sprites
                         if(colorNum == 0)
                             continue;
 
-                        unsigned char red = 0;
-                        unsigned char blue = 0;
-                        unsigned char green = 0;
+                        uint8_t red = 0;
+                        uint8_t blue = 0;
+                        uint8_t green = 0;
 
                         switch(color)
                         {
@@ -384,12 +417,12 @@ class GB_GPU {
             }
         }
 
-        unsigned char getColor(int colorNum, unsigned short address)
+        uint8_t getColor(int colorNum, uint16_t address)
         {
-            unsigned char color = 0x00; //Default white
-            unsigned char palette = memory->read(address);
-            unsigned char hi = 0;
-            unsigned char lo = 0;
+            uint8_t color = 0x00; //Default white
+            uint8_t palette = memory->read(address);
+            uint8_t hi = 0;
+            uint8_t lo = 0;
             
             //Bits of the palette the color ID matches to
             switch(colorNum)
